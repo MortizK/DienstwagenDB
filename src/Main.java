e15558eeebbb41b4f59731115c72821182941d4a
MortizK <moritz-koehler@freenet.de>
2025-06-25 19:33:17 +0200

    Better --help examples and args now goes through all commands

--- src/Main.java
+++ src/Main.java
@@ -18,35 +18,37 @@ public class Main {
             return;
         }
 
-        String[] parts = args[0].replace("\"", "").split("=");
+        for (String arg : args) {
+            String[] parts = arg.replace("\"", "").split("=");
 
-        switch (parts[0]) {
-            case "--fahrersuche":
-                List<Driver> drivers = data.getDriversByString(parts[1]);
-                System.out.println(drivers);
-                break;
-            case "--fahrzeugsuche":
-                List<Car> cars = data.getCarsByString(parts[1]);
-                System.out.println(cars);
-                break;
-            case "--fahrerZeitpunkt":
-                String speeding = data.getDriverSpeeding(parts[1], ";");
-                System.out.println(speeding);
-                break;
-            case "--fahrerDatum":
-                String driversOfDay = data.getDriversOfDay(parts[1], ";");
-                System.out.println(driversOfDay);
-                break;
-            case "--help":
-                System.out.println("Possible commands");
-                System.out.println("--fahrersuche=\"Hoff\"");
-                System.out.println("--fahrzeugsuche=\"Passat\"");
-                System.out.println("--fahrerZeitpunkt=\"S-MN-9932;2024-01-01T14:00:00\"");
-                System.out.println("--fahrerDatum=\"F003;2024-01-01\"");
-                System.out.println("--help");
-                break;
-            default:
-                System.out.println("Invalid command, use '--help' for help");
+            switch (parts[0]) {
+                case "--fahrersuche":
+                    List<Driver> drivers = data.getDriversByString(parts[1]);
+                    System.out.println(drivers);
+                    break;
+                case "--fahrzeugsuche":
+                    List<Car> cars = data.getCarsByString(parts[1]);
+                    System.out.println(cars);
+                    break;
+                case "--fahrerZeitpunkt":
+                    String speeding = data.getDriverSpeeding(parts[1], ";");
+                    System.out.println(speeding);
+                    break;
+                case "--fahrerDatum":
+                    String driversOfDay = data.getDriversOfDay(parts[1], ";");
+                    System.out.println(driversOfDay);
+                    break;
+                case "--help":
+                    System.out.println("Possible commands");
+                    System.out.println("--fahrersuche=\"Hoff\"");
+                    System.out.println("--fahrzeugsuche=\"Passat\"");
+                    System.out.println("--fahrerZeitpunkt=\"S-CD-4737;2024-01-01T14:00:00\"");
+                    System.out.println("--fahrerDatum=\"F001;2024-01-01\"");
+                    System.out.println("--help");
+                    break;
+                default:
+                    System.out.println("Invalid command, use '--help' for help");
+            }
         }
     }
 }
